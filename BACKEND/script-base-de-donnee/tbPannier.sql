0f46f9f16173c2f421ec02983e433cb1b8b5d467
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-07-28 13:56:43 +0200

    Update tbPannier.sql

--- BACKEND/script-base-de-donnee/tbPannier.sql
+++ BACKEND/script-base-de-donnee/tbPannier.sql
@@ -1,5 +1,6 @@
 CREATE TABLE tbPannier(
 	panniId INT NOT NULL, 
 	nomVet VARCHAR(50) NOT NULL, 
-	constraint pk_tbPannier PRIMARY KEY (panniId)
+	constraint pk_tbPannier PRIMARY KEY (panniId),
+   	CONSTRAINT fk_tbPannier_tbNom FOREIGN KEY (panniId) REFERENCE tbNom (nomId)
 );
