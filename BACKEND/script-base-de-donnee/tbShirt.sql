da9a25e42527eb9924de51083532bdb2d679628d
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-07-28 13:49:41 +0200

    Update tbShirt.sql

--- BACKEND/script-base-de-donnee/tbShirt.sql
+++ BACKEND/script-base-de-donnee/tbShirt.sql
@@ -3,13 +3,9 @@ CREATE TABLE tbShirt(
 	nomId INT NOT NULL, 
 	tailleId INT NOT NULL, 
 	couleurId INT NOT NULL, 
-    	connecId INT NOT NULL,
-   	panniId INT NOT NULL,
 	shirtPrix DECIMAL (4,2) NULL,
 	constraint pk_tbShirt PRIMARY KEY (shirtId),
 	CONSTRAINT fk_tbShirt_tbCouleur FOREIGN KEY (couleurId) REFERENCES tbCouleur (couleurId),
 	CONSTRAINT fk_tbShirt_tbNom FOREIGN KEY (nomId) REFERENCES tbNom (nomId),
-	CONSTRAINT fk_tbShirt_tbTaille FOREIGN KEY (tailleId) REFERENCES tbTaille (tailleId),
-    	CONSTRAINT fk_tbShirt_tbConnect FOREIGN KEY (connecId) REFERENCES tbConnect (connecId),
-    	CONSTRAINT fk_tbShirt_tbPannier FOREIGN KEY (panniId) REFERENCES tbPannier (panniId)
+	CONSTRAINT fk_tbShirt_tbTaille FOREIGN KEY (tailleId) REFERENCES tbTaille (tailleId)
 ); 
