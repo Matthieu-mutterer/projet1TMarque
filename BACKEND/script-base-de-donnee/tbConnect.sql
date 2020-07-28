895e6d560f4b441876e0eaca9984e19a3b885ee5
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-07-28 14:24:10 +0200

    Update tbConnect.sql

--- BACKEND/script-base-de-donnee/tbConnect.sql
+++ BACKEND/script-base-de-donnee/tbConnect.sql
@@ -2,5 +2,7 @@ CREATE TABLE tbConnect(
 	connecId INT NOT NULL, 
 	adresse VARCHAR(100) NOT NULL, 
 	passw VARCHAR(25) NOT NULL,
-	constraint pk_tbConnect PRIMARY KEY (connecId)
+	constraint pk_tbConnect PRIMARY KEY (connecId), 
+    constraint fk_tbConnect_tbPannier FOREIGN KEY (connecId) REFERENCES tbPannier (panniId)
+    
 );
