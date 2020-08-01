2d936b24db9002da1d4aee5b5cf3919eed7f66aa
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-01 15:59:19 +0200

    Update proc-serviceWeb.sql

--- BACKEND/script-serveur-web/proc-serviceWeb.sql
+++ BACKEND/script-serveur-web/proc-serviceWeb.sql
@@ -18,3 +18,10 @@ BEGIN
     call sa_set_http_header('Content-Type','text/html');
     select xp_read_file(dba.getPath() || url || '.html'); /* renvoyer page */ 
 END;  
+
+CREATE PROCEDURE "DBA"."http_getIMG"(in url char(255)) /* renvoie le contenu de l image dont le nom (+ extension) est le paramètre url */
+BEGIN 
+    call sa_set_http_header('Content-Type', 'image/png'); 
+    select xp_read_file(dba.getPath() || 'IMG\' || url); // revoyer image 
+END; 
+    
