61b96562891460ee08a4f140e8d5ecc267642faa
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-01 16:05:15 +0200

    Update proc-serviceWeb.sql

--- BACKEND/script-serveur-web/proc-serviceWeb.sql
+++ BACKEND/script-serveur-web/proc-serviceWeb.sql
@@ -24,4 +24,11 @@ BEGIN
     call sa_set_http_header('Content-Type', 'image/png'); 
     select xp_read_file(dba.getPath() || 'IMG\' || url); // revoyer image 
 END; 
+
+CREATE PROCEDURE "DBA"."http_getJS"(in url char(255))
+RESULT (js long varchar)
+BEGIN 
+    call sa_set_http_header('Content-Type', 'application/javascript'); /* header http */
+    select xp_read_file(dba.getPath() || 'js\' || url); /* renvoyer fichier javascript */
+END; 
     
