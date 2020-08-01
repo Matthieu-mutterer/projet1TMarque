7820a656be95ecfae8e87c2ccd564ae8c54f1084
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-01 15:53:20 +0200

    Update proc-serviceWeb.sql

--- BACKEND/script-serveur-web/proc-serviceWeb.sql
+++ BACKEND/script-serveur-web/proc-serviceWeb.sql
@@ -11,3 +11,10 @@ BEGIN
 
     return dbPath; /* renvoyer path */
 END; 
+
+CREATE PROCEDURE "DBA"."http_getPage"(in url char(255))/* renvoie le contenu de la page html dont le nom est le parametre url */
+RESULT(html long varchar)
+BEGIN  
+    call sa_set_http_header('Content-Type','text/html');
+    select xp_read_file(dba.getPath() || url || '.html'); /* renvoyer page */ 
+END;  
