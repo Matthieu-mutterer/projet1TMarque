9abf863e0afc3309611bf0bc66d10a05356e034f
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-02 11:21:56 +0200

    Update proc-serviceWeb.sql

--- BACKEND/script-serveur-web/proc-serviceWeb.sql
+++ BACKEND/script-serveur-web/proc-serviceWeb.sql
@@ -43,3 +43,4 @@ ________________________________________________________________________________
 
 CREATE SERVICE "page" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getPage(:url); 
 CREATE SERVICE "js" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getJS(:url); 
+CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getCSS(:url); 
