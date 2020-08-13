183d845fd97f32a45f69180621dffc32eaa9b76a
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-13 15:26:48 +0200

    Update getPage.sql

--- BACKEND/script-serveur-web/getPage.sql
+++ BACKEND/script-serveur-web/getPage.sql
@@ -6,4 +6,5 @@ BEGIN
 END
 --------------------------------------------------------------------------------------------------------------------------------------------
 CREATE SERVICE "page" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getPage(:url);
+CREATE SERVICE "root" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getPage('projet');
 
