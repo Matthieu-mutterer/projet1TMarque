b6ab93ae326f3cb45292d728d0bcbfc654c98d61
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-13 15:17:06 +0200

    Create getIMG.sql

---
+++ BACKEND/script-serveur-web/getIMG.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE "DBA"."http_getIMG"(in url char(255)) /* renvoie le contenu de l image dont le nom (+ extension) est le paramètre url */
+BEGIN 
+    call sa_set_http_header('Content-Type', 'image/png'); 
+    select xp_read_file(dba.getPath() || 'IMG\' || url); // revoyer image 
+END
+---------------------------------------------------------------------------------------------------------------------------------------------
