959a59383b968c9b7a6ea34f4465c17b091dff97
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-13 15:27:44 +0200

    Delete getIMG.sql

--- BACKEND/script-serveur-web/getIMG.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE "DBA"."http_getIMG"(in url char(255)) /* renvoie le contenu de l image dont le nom (+ extension) est le paramètre url */
-BEGIN 
-    call sa_set_http_header('Content-Type', 'image/png'); 
-    select xp_read_file(dba.getPath() || 'IMG\' || url); // revoyer image 
-END
----------------------------------------------------------------------------------------------------------------------------------------------
-CREATE SERVICE "img" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getIMG(:url);
