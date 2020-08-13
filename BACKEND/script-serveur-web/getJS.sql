703994e7765d1572d22370d7677884317d189277
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-13 15:27:52 +0200

    Delete getJS.sql

--- BACKEND/script-serveur-web/getJS.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE "DBA"."http_getJS"(in url char(255))
-RESULT (js long varchar)
-BEGIN 
-    call sa_set_http_header('Content-Type', 'application/javascript'); /* header http */
-    select xp_read_file(dba.getPath() || 'js\' || url); /* renvoyer fichier javascript */
-END
----------------------------------------------------------------------------------------------
-CREATE SERVICE "js" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getJS(:url);
