d348e6fdf1cd890dc79787c24edf0f8003898b88
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-13 15:27:32 +0200

    Delete getCSS.sql

--- BACKEND/script-serveur-web/getCSS.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE "DBA"."http_getCSS"(in url char(255))
-RESULT (css long varchar)
-BEGIN
-	call sa_set_http_header('Content-Type', 'text/css');
-	select xp_read_file(dba.getPath() || 'css\' || url);
-END
--------------------------------------------------------------
-CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call dba.http_getCSS(:url);
-
