119554eab8d3e8f80d13dc88d41a722abec3ca87
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-19 11:26:02 +0200

    Create proc_insert_connecter.sql

---
+++ BACKEND/script-api-rest/proc_insert_connecter.sql
@@ -0,0 +1,7 @@
+ALTER PROCEDURE "DBA"."proc_insert_connecter"(in adres varchar(100), in pasw varchar(25))
+BEGIN
+  INSERT INTO tbConnect (adresse, passw)
+  VALUES (adres, pasw);
+END
+------------------------------------------------------------------------------------------------
+CREATE SERVICE "proc_insert_connecter" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call dba.proc_insert_connecter(:adres,:pasw);
