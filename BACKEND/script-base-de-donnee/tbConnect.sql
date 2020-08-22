1c1ca822c5b51da7e15d96a835aeb0eab1efe9ea
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-22 11:17:23 +0200

    Update tbConnect.sql

--- BACKEND/script-base-de-donnee/tbConnect.sql
+++ BACKEND/script-base-de-donnee/tbConnect.sql
@@ -1,7 +1,6 @@
 CREATE  TABLE tbConnect(
 	connecId INT NOT NULL DEFAULT AUTOINCREMENT , 
 	adresse VARCHAR (100) NOT NULL, 
-	passw VARCHAR(8) NOT NULL,
-   	panniId INT NOT NULL, 
+	passw VARCHAR(50) NOT NULL, 
 	constraint pk_tbConnect PRIMARY KEY (connecId)
 )
