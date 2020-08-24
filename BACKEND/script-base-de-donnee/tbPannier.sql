5db031b8ee239db95a1aeefcef4b09952fc92c0b
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-24 16:47:17 +0200

    Update tbPannier.sql

--- BACKEND/script-base-de-donnee/tbPannier.sql
+++ BACKEND/script-base-de-donnee/tbPannier.sql
@@ -1,10 +1,7 @@
 CREATE TABLE tbPannier(
-	panniId INT NOT NULL,
-   connecId INT NOT NULL,  
+	panniId INT NOT NULL,  
 	nomVet VARCHAR(50) NOT NULL, 
 	nomCouleur VARCHAR(50) NOT NULL,
 	nomTAil varchar(50) NOT NULL,
 	shirtPrix INT NOT NULL, 
-	constraint pk_tbConnect PRIMARY KEY (panniId), 
-    CONSTRAINT fk_tbPannier_tbConnect FOREIGN KEY (connecId) REFERENCES tbConnect (connecId),
 );
