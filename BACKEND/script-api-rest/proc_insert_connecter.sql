709fefe54cf2b4bac2ca41f20ab7ad8a61edcdf7
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-24 16:50:24 +0200

    Update proc_insert_connecter.sql

--- BACKEND/script-api-rest/proc_insert_connecter.sql
+++ BACKEND/script-api-rest/proc_insert_connecter.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE "DBA"."proc_insert_connecter"(in adres varchar(100), in pasw varchar(25))
+CREATE PROCEDURE "DBA"."proc_insert_connecter"(in adres varchar(100), in pasw varchar(25))
 BEGIN
   INSERT INTO tbConnect (adresse, passw)
   VALUES (adres, pasw);
