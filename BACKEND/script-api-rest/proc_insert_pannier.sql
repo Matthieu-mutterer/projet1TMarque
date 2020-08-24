01e10d3e0805dcee4c94ab110c731449ed76e0e6
Matthieu-mutterer <64152324+Matthieu-mutterer@users.noreply.github.com>
2020-08-24 16:49:53 +0200

    Update proc_insert_pannier.sql

--- BACKEND/script-api-rest/proc_insert_pannier.sql
+++ BACKEND/script-api-rest/proc_insert_pannier.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE "DBA"."proc_insert_pannier"(in nom varchar(50), in coul varchar(50), in tail varchar(50),in prix integer)
+ALTER PROCEDURE "DBA"."proc_insert_pannier"(in nom varchar(50), in coul varchar(50), in tail varchar(50),in prix integer)
 BEGIN
   INSERT INTO tbPannier (nomVet, nomCouleur, nomTAil, shirtPrix)
   VALUES (nom, coul, tail, prix);
